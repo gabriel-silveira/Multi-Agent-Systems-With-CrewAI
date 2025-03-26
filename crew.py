fe5890a47c8f0dbad0b2547ffece256b522bb64c
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-03-26 16:42:28 -0300

    Refactoring on content writer crew

--- crew.py
+++ crew.py
@@ -1,11 +1,10 @@
-from crewai import Crew
-
 from agents.editor import editor
 from agents.planner import planner
 from agents.writer import writer
 from tasks.edit import edit
 from tasks.plan import plan
 from tasks.write import write
+from crewai import Crew
 
 crew = Crew(
   agents=[planner, writer, editor],
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import warnings
 import os
+
 from utils.utils import get_openai_api_key
 
 warnings.filterwarnings('ignore')
 
 openai_api_key = get_openai_api_key()
 os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
+
+topic = input("What content would you like me to create?\n")
+
+from crew import crew
+
+result = crew.kickoff(inputs={"topic": topic})
